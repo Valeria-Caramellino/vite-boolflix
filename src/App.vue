f1bb30af84b5079092e095e6de704f80a9943f7e
valeria caramellino <valeriacaramellino@gmail.com>
2023-05-26 15:23:41 +0200

    sistemazione sezione bandiere

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import AppMain from './components/AppMain.vue';
 import AppHeader from './components/AppHeader.vue';
-
+import  "/node_modules/flag-icons/css/flag-icons.min.css" ;
 export default{
   name: "App",
   components:{
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,6 @@
 <script >
 
-
+import  "/node_modules/flag-icons/css/flag-icons.min.css" ;
 import { store } from '../data/store';
 import axios from 'axios';
 
@@ -34,7 +34,10 @@ export default{
           this.store.ArraySerie = oggetto.data;
           });
        
-        }
+        },
+        getFlag(nazione) {
+            return `fi fi-${this.store.ForFlag[nazione]}`
+        },
     },
     mounted() {
     },
@@ -43,34 +46,35 @@ export default{
 </script>
 
 <template>
- 
-  
-  <!--
-  <pre>{{ this.store.Array.results }}</pre>
--->
-  
+  <!--<pre>{{ this.store.Array.results }}</pre>-->
     <div class="container">
       <div class="row">
         <div class="col-12 m-2">
           <input v-model="testo" type="text" @keyup="change()" placeholder="Cerca">
-          <button @click="change()" class="m-2">Lista iniziale</button>
         </div>
         <div class="col-12 bg-danger my-2">
           <h2>LISTA FILM</h2>
         </div>
+<!--section film-->
         <div class="col-12 d-flex flex-wrap justify-content-around">
           <template v-for="oggetto in store.ArrayFilm.results">
             
           <div class="col-3 m-2 p-2">
+            <div>
+              <img :src=this.store.inizioImmagine+oggetto.poster_path alt="">
+            </div>
+            <div>
+              <p>Titolo:</p>
+              <span>{{ oggetto.title }}</span>
+              <!--<p>Titolo originale: {{ oggetto.original_title }}</p> -->
+          
+              <p>Lingua:</p>
+              <span :class="getFlag(oggetto.original_language)"></span>
+              
+              <p>Voto:</p>
+              <span>{{ oggetto.vote_average }}</span>
+            </div>
             
-              <img :src=this.store.inizioImmagine+oggetto.backdrop_path alt="">
-            
-            
-            <p>Titolo:{{ oggetto.title }} </p>
-            <!--
-            <p>Titolo originale: {{ oggetto.original_title }}</p>-->
-            <img :src =" `https://flagcdn.com/w20/${oggetto.original_language}.png`" alt="" class="flag">
-            <p>Voto: {{ oggetto.vote_average }}</p>
           </div>
           </template>
         </div>
@@ -79,16 +83,24 @@ export default{
         <div class="col-12 bg-danger my-2">
           <h2>LISTA TELEFILM</h2>
         </div>
+<!--section telefilm-->
         <div class="col-12 d-flex flex-wrap justify-content-around">
           <template v-for="oggetto in store.ArraySerie.results">
             
             <div class="col-3 m-2 p-2">
-                <img :src=this.store.inizioImmagine+oggetto.backdrop_path alt="">
-                <p>Titolo:{{ oggetto.name }} </p>
-            <!--
-                <p>Titolo originale: {{ oggetto.original_name }}</p>-->
-                <img :src =" `https://flagcdn.com/w20/${oggetto.original_language}.png`" alt="" class="flag">
-                <p>Voto: {{ oggetto.vote_average }}</p>
+              <div>
+                <img :src=this.store.inizioImmagine+oggetto.poster_path alt="">
+              </div>
+              <div>
+                <p>Titolo:</p>
+                <span>{{ oggetto.name }} </span>
+                <!--<p>Titolo originale: {{ oggetto.original_name }}</p>-->
+                <p>Lingua:</p>
+                <span :class="getFlag(oggetto.original_language)"></span>
+                <p>Voto:</p>
+                <span>{{ oggetto.vote_average }}</span>
+              </div>
+                
             </div>
           </template>
         </div>
